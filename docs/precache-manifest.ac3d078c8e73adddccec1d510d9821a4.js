4c9cf4acfd87d1a3480294dc7022413a16519ca9
Ramsés Mendoza <mg.ramses@gmail.com>
2020-09-28 21:07:25 -0500

    docs agregado

---
+++ docs/precache-manifest.ac3d078c8e73adddccec1d510d9821a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28413ecd5fd0f58e4f1978b37ec81504",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "0255bba7173de7b24a7c",
+    "url": "/pomodoro-timer/static/css/main.df045ce8.chunk.css"
+  },
+  {
+    "revision": "73b8122066788603c24c",
+    "url": "/pomodoro-timer/static/js/2.f889f1d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-timer/static/js/2.f889f1d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0255bba7173de7b24a7c",
+    "url": "/pomodoro-timer/static/js/main.2ce840d8.chunk.js"
+  },
+  {
+    "revision": "5b934f73dc3f50a7b525",
+    "url": "/pomodoro-timer/static/js/runtime-main.cb3a558a.js"
+  }
+]);
